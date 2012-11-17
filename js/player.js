d1865b7e3c5985ac4ed7131855681a73930db513
Benjamin Holbrook <holbrook.benjamin@gmail.com>
2012-11-17 15:49:34 +0800

    Update collisions to check inner ship circle. Updated Player with modified inner/outer circle functions/defaults

--- js/player.js
+++ js/player.js
@@ -1,8 +1,8 @@
 
-var DEFAULT_SHIP_COLOR =			"#00FFFF";
-var DEFAULT_INNER_SHIP_COLOR =		"#000000";
-var DEFAULT_SHIP_RADIUS = 			10;
-var DEFAULT_SHIP_INNER_RADIUS =		2;
+var DEFAULT_SHIP_OUTER_COLOR =		"#00FFFF";
+var DEFAULT_SHIP_COLOR =			"#000000";
+var DEFAULT_SHIP_OUTER_RADIUS = 	10;
+var DEFAULT_SHIP_RADIUS =			2;
 
 var PLAYER_DEATHS = 0;
 
@@ -10,16 +10,16 @@ function Player() {
 	this.xpos =				250;
 	this.ypos = 			450;
 	this.radius = 			DEFAULT_SHIP_RADIUS;
-	this.innerRadius =		DEFAULT_SHIP_INNER_RADIUS;
+	this.outerRadius =		DEFAULT_SHIP_OUTER_RADIUS;
 	this.color = 			DEFAULT_SHIP_COLOR;
-	this.innerColor = 		DEFAULT_INNER_SHIP_COLOR;
+	this.outerColor = 		DEFAULT_SHIP_OUTER_COLOR;
 	
 	this.getX = 			function() { return this.xpos; }
 	this.getY = 			function() { return this.ypos; }
 	this.getRadius = 		function() { return this.radius; }
-	this.getInnerRadius = 	function() { return this.innerRadius; }
+	this.getOuterRadius = 	function() { return this.outerRadius; }
 	this.getColor = 		function() { return this.color; }
-	this.getInnerColor = 	function() { return this.innerColor; }
+	this.getOuterColor = 	function() { return this.outerColor; }
 	
 	this.update = function(x, y) {
 		this.updatePosition(x, y);
@@ -31,16 +31,16 @@ function Player() {
 	}
 	
 	this.redraw = function(x, y) {
-		ctx.fillStyle = this.getColor();
+		ctx.fillStyle = this.getOuterColor();
 		ctx.beginPath();
-		ctx.arc(this.getX(), this.getY(), this.getRadius(), 0, Math.PI * 2, true);
+		ctx.arc(this.getX(), this.getY(), this.getOuterRadius(), 0, Math.PI * 2, true);
 		ctx.closePath();
 		ctx.fill();
 		
 		// Draw inner, smaller circle
-		ctx.fillStyle = this.getInnerColor();
+		ctx.fillStyle = this.getColor();
 		ctx.beginPath();
-		ctx.arc(this.getX(), this.getY(), this.getInnerRadius(), 0, Math.PI * 2, true);
+		ctx.arc(this.getX(), this.getY(), this.getRadius(), 0, Math.PI * 2, true);
 		ctx.closePath();
 		ctx.fill();
 	}
